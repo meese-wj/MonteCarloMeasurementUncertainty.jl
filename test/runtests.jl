62c080de11ffd42efe66713d0a8d2c4c03d331e7
W. Joe Meese <meese022@umn.edu>
2022-07-11 15:36:20 -0500

    Add measurement unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,20 @@ using Documenter
         end
     end
 
+    @testset "Measurements" begin
+        
+        for obs_type ∈ (:TimeSeries, :AccumulatedSeries)
+            eval(quote
+                acc = $obs_type()
+                for idx ∈ 1:Int(2^18) push!(acc, idx % 512) end
+                meas = measurement(acc)
+                @test meas.val ≈ 255.5 
+                @test isapprox(meas.err, 2.264245; atol = 1e-6)
+            end)
+        end
+
+    end
+
     @testset "Doctests" begin
         DocMeta.setdocmeta!(MonteCarloMeasurements, :DocTestSetup, :(using MonteCarloMeasurements); recursive=true)
         doctest(MonteCarloMeasurements)
